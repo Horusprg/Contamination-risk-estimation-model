21a11c3172eeae267f5b7c7d8ce9e8ca617efb23
Horusprg <62734053+Horusprg@users.noreply.github.com>
2022-02-22 20:13:17 -0300

    make it better

--- web.py
+++ web.py
@@ -1,19 +1,25 @@
 from distutils.log import debug
-from flask import Flask, Response, request, redirect
+from flask import Flask, Response, request, redirect, render_template, make_response
+from psutil import users
 from detection import VideoCamera, gen
 
-app = Flask(__name__, static_folder= "static")
+app = Flask(__name__, template_folder="templates")
 @app.route('/')
 def index():
-    return redirect("/static/index.html")
+    return render_template("index.html")
 
-@app.route('/video_feed', methods=["POST"])
+@app.route('/detect', methods=["POST"])
+def detect():
+    resp = make_response(render_template("detect.html"))
+    url = request.form['url']
+    resp.set_cookie("url", url)
+    return resp
+
+@app.route('/video_feed', methods=["GET"])
 def video_feed():
-    if request.method == "POST":
-        url = request.form["url"]
-        return Response(gen(VideoCamera(url)),
+    if request.cookies.get("url") != 'none':
+        return Response(gen(VideoCamera(request.cookies.get("url"))),
                         mimetype='multipart/x-mixed-replace; boundary=frame')
-    return "INVALID ACESS"
 
 if __name__ == '__main__':
     app.run(debug=True)
