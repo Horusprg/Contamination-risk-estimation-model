ba9844a1076f1b76f596c2763f8a134699e751df
Horusprg <62734053+Horusprg@users.noreply.github.com>
2022-02-22 22:41:30 -0300

    Size adjust

--- web.py
+++ web.py
@@ -1,12 +1,13 @@
-from distutils.log import debug
-from flask import Flask, Response, request, redirect, render_template, make_response
-from psutil import users
+from flask import Flask, Response, request, render_template, make_response
 from detection import VideoCamera, gen
 
 app = Flask(__name__, template_folder="templates")
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 @app.route('/detect', methods=["POST"])
 def detect():
@@ -15,11 +16,13 @@ def detect():
     resp.set_cookie("url", url)
     return resp
 
+
 @app.route('/video_feed', methods=["GET"])
 def video_feed():
     if request.cookies.get("url") != 'none':
         return Response(gen(VideoCamera(request.cookies.get("url"))),
                         mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
